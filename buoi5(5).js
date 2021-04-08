15df991e57be88236834da14ad6229d194cd9c60
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2021-04-08 22:49:37 +0700

    Add files via upload

---
+++ buoi5(5).js
@@ -0,0 +1,5 @@
+console.log("Solving the equation: ax+b=0")
+let first_Num = prompt("Insert a")
+let second_Num = prompt("Insert b")
+let root = (0- second_Num)/first_Num
+console.log("The root of your equation is: " + root)
