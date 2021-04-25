416001b7287cd1a17f693643b0f8e749ff6defc9
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2021-04-25 12:09:55 +0700

    Update newWeb.js

--- newWeb.js
+++ newWeb.js
@@ -2,14 +2,14 @@ document.getElementById("login_Box").addEventListener("click", on_Click)
 function on_Click(){
     let x = document.getElementById("email").value;
     let y = document.getElementById("password").value;
+    if (x === 'example@gmail.com' && y === '123'){
+        check();
+    }
+    else{
+        alert("Wrong");
+    }
 }
 function check(){
     document.getElementById("page").style.display="none";
     document.getElementById("second_Page").style.display="block";
-}
-if (x === 'example@gmail.com' && y === '123'){
-    check();
-}
-else{
-    alert("Wrong");
 }
